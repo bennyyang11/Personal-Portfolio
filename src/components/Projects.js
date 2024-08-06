fdcfde057a9a3419ca7a6034899f7cfe48777497
bennyyang11 <bennyyang11@gmail.com>
2024-08-05 22:42:10 -0600

    Fixing the projects tab section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,62 +1,97 @@
-import { Col, Container, Row } from "react-bootstrap";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import TrackVisibility from 'react-on-screen';
 
 export const Projects = () => {
-    const responsive = {
-        superLargeDesktop: {
-            breakpoint: { max:4000, min:3000 },
-            items: 5
+    
+    const projects = [
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg1,
         },
-        desktop: { 
-            breakpoint: { max: 3000, min: 1024 },
-            items: 3
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg2,
         },
-        tablet: {
-            breakpoint: {max: 1024, min:464 },
-            items: 2
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg3,
         },
-        mobile: {
-            breakpoint: {max: 464, min: 0 },
-            items: 1
-        }
-    };
-    return (
-        <section className="projects" id="projects">
-            <Container>
-                <Row>
-                    <Col>
-                     <div className="project-bx">
-                        <h2>
-                            Project
-                        </h2>
-                        <p>Below will be my projects</p>
-                        <Carousel responsive={responsive} infinite={true} className="project-slider">
-                            <div className = "item">
-                                <img src = {''} alt = "Image" />
-                                <h5>project 1 </h5>
-                            </div>
-                            <div className = "item">
-                                <img src = {''} alt = "Image" />
-                                <h5>project 2 </h5>
-                            </div>
-                            <div className = "item">
-                                <img src = {''} alt = "Image" />
-                                <h5>project 3 </h5>
-                            </div>
-                            <div className = "item">
-                                <img src = {''} alt = "Image" />
-                                <h5>project 4 </h5>
-                            </div>
-                            <div className = "item">
-                                <img src = {''} alt = "Image" />
-                                <h5>project 5 </h5>
-                            </div>
-                        </Carousel>
-                     </div>
-                    </Col>
-                </Row>
-            </Container>
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg1,
+        },
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg2,
+        },
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg3,
+        },
+      ];
+    
+      return (
+        <section className="project" id="projects">
+          <Container>
+            <Row>
+              <Col size={12}>
+                <TrackVisibility>
+                  {({ isVisible }) =>
+                  <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                    <h2>Projects</h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                      <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                        <Nav.Item>
+                          <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                        </Nav.Item>
+                      </Nav>
+                      <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                        <Tab.Pane eventKey="first">
+                          <Row>
+                            {
+                              projects.map((project, index) => {
+                                return (
+                                  <ProjectCard
+                                    key={index}
+                                    {...project}
+                                    />
+                                )
+                              })
+                            }
+                          </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="section">
+                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="third">
+                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                        </Tab.Pane>
+                      </Tab.Content>
+                    </Tab.Container>
+                  </div>}
+                </TrackVisibility>
+              </Col>
+            </Row>
+          </Container>
+          <img className="background-image-right" src={colorSharp2}></img>
         </section>
-    )
+      )
 }
