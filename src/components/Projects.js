3cd70e564d1fea76071451a5422db4aeb959880c
bennyyang11 <bennyyang11@gmail.com>
2024-08-05 22:51:41 -0600

    Fixing the tabs on project section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,35 +10,20 @@ export const Projects = () => {
     
     const projects = [
         {
-          title: "Business Startup",
-          description: "Design & Development",
+          title: "Project Name",
+          description: "Type of project",
           imgUrl: projImg1,
         },
         {
-          title: "Business Startup",
-          description: "Design & Development",
-          imgUrl: projImg2,
-        },
-        {
-          title: "Business Startup",
-          description: "Design & Development",
-          imgUrl: projImg3,
-        },
-        {
-          title: "Business Startup",
-          description: "Design & Development",
-          imgUrl: projImg1,
-        },
-        {
-          title: "Business Startup",
-          description: "Design & Development",
-          imgUrl: projImg2,
-        },
-        {
-          title: "Business Startup",
-          description: "Design & Development",
-          imgUrl: projImg3,
-        },
+            title: "Project Name",
+            description: "Type of project",
+            imgUrl: projImg2,
+          },
+          {
+            title: "Project Name",
+            description: "Type of project",
+            imgUrl: projImg3,
+          },
       ];
     
       return (
@@ -50,7 +35,7 @@ export const Projects = () => {
                   {({ isVisible }) =>
                   <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                     <h2>Projects</h2>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <p>General Project description about tabs</p>
                     <Tab.Container id="projects-tabs" defaultActiveKey="first">
                       <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                         <Nav.Item>
@@ -78,11 +63,45 @@ export const Projects = () => {
                             }
                           </Row>
                         </Tab.Pane>
+                        <Tab.Pane eventKey="second">
+                          <Row>
+                            {
+                              projects.map((project, index) => {
+                                return (
+                                  <ProjectCard
+                                    key={index}
+                                    {...project}
+                                    />
+                                )
+                              })
+                            }
+                          </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="third">
+                          <Row>
+                            {
+                              projects.map((project, index) => {
+                                return (
+                                  <ProjectCard
+                                    key={index}
+                                    {...project}
+                                    />
+                                )
+                              })
+                            }
+                          </Row>
+                        </Tab.Pane>
                         <Tab.Pane eventKey="section">
                           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
                         </Tab.Pane>
+                        <Tab.Pane eventKey="first">
+                          <p>Project description</p>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="second">
+                          <p>Project description</p>
+                        </Tab.Pane>
                         <Tab.Pane eventKey="third">
-                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                          <p>Project description</p>
                         </Tab.Pane>
                       </Tab.Content>
                     </Tab.Container>
