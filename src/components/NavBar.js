efbe97ca262cba2dedd51bc07508a73d25782af2
bennyyang11 <bennyyang11@gmail.com>
2025-05-29 00:15:18 -0600

    fix linkedIn shortcut

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -62,9 +62,12 @@ export const NavBar = () => {
                     </Nav>
                     <span className="navbar-text">
                         <div className="social-icon">
-                            <a href="#"><img src={navIcon1} alt="" /></a>
-                            <a href="#"><img src={navIcon2} alt="" /></a>
-                            <a href="#"><img src={navIcon3} alt="" /></a>
+                        <a href="https://www.linkedin.com/in/byangln" target="_blank" rel="noopener noreferrer">
+                            <img src={navIcon1} alt="LinkedIn" />
+                        </a>
+                             {/*<a href="#"><img src={navIcon1} alt="www.linkedin.com/in/byangln" /></a>
+                            {/*<a href="#"><img src={navIcon2} alt="" /></a>
+                            <a href="#"><img src={navIcon3} alt="" /></a>*/}
                         </div>
                         <button className="vvd" onClick={() => console.log('connect')}>
                             <span>Let's Connect</span>
